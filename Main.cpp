3b561ce5b41fc507c0bfe2f8d960a406a57d2ae7
AayushBade14 <aayushrajubade@gmail.com>
2025-06-09 20:39:42 +0530

    Basic tilemaps implemented

--- Main.cpp
+++ Main.cpp
@@ -10,7 +10,28 @@
 #define HEIGHT 1080
 #define TITLE "LiveToSurvive"
 
+#define WORLD_X 5
+#define WORLD_Y 5
+
+float uv_x = 0.0f;
+float uv_y = 0.0f;
+
 void framebuffer_size_callback(GLFWwindow *window,int width,int height);
+ 
+void decideTile(int t){
+  if(t == 2){
+    uv_x = 0.0f;
+    uv_y = 13.0f;
+  }
+  else if(t == 1){
+    uv_x = 0.0f;
+    uv_y = 12.0f;
+  }
+  else if(t == 3){
+    uv_x = 9.0f;
+    uv_y = 0.0f;
+  }
+}
 
 int main(void){
   if(glfwInit()<0){
@@ -50,6 +71,14 @@ int main(void){
     -50.0f,-50.0f,0.0f, 0.0f,0.0f
   };
   
+  float tilemap[WORLD_X][WORLD_Y] = {
+    {1,1,1,1,1},
+    {1,2,2,2,1},
+    {1,2,3,2,1},
+    {1,2,2,2,1},
+    {1,1,1,1,1}
+  };
+
   unsigned int VAO,VBO;
 
   glGenVertexArrays(1,&VAO);
@@ -102,8 +131,10 @@ int main(void){
   float frame_ps = 8.0f;
   float nxf = 25.0f;
   float nyf = 13.0f;
-  float uv_x = 0.0f;
-  float uv_y = 0.0f;
+  
+  float tileSize = 100.0f;
+  float offsetX = 760.0f;
+  float offsetY = 340.0f;
 
   while(!glfwWindowShouldClose(window)){
     float currentFrame = (float)glfwGetTime();
@@ -133,23 +164,33 @@ int main(void){
     }
 
     glm::mat4 model = glm::mat4(1.0f);
-    model = glm::translate(model,glm::vec3((float)WIDTH/2,(float)HEIGHT/2,0.0f));
 
     glm::mat4 view = glm::mat4(1.0f);
 
-    glm::mat4 projection = glm::ortho(0.0f,(float)WIDTH,0.0f,(float)HEIGHT);
+    glm::mat4 projection = glm::ortho((float)WIDTH,0.0f,(float)HEIGHT,0.0f);
 
     shader.use();
-    shader.setValue("model",model);
-    shader.setValue("view",view);
-    shader.setValue("projection",projection);
-    shader.setValue("uv_x",uv_x);
-    shader.setValue("uv_y",uv_y);
-    shader.setValue("nxf",nxf);
-    shader.setValue("nyf",nyf);
 
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES,0,6);
+    //glDrawArrays(GL_TRIANGLES,0,6);
+    for(int x = 0; x < WORLD_X; x++){
+      for(int y = 0; y < WORLD_Y; y++){
+        model = glm::mat4(1.0f);
+        model = glm::translate(model,glm::vec3(offsetX+(x*tileSize),offsetY+(y*tileSize),0.0f));
+        decideTile(tilemap[x][y]);
+        shader.setValue("model",model);
+        shader.setValue("view",view);
+        shader.setValue("projection",projection);
+        shader.setValue("uv_x",uv_x);
+        shader.setValue("uv_y",uv_y);
+        shader.setValue("nxf",nxf);
+        shader.setValue("nyf",nyf);
+       
+        if(tilemap[x][y] != 0)
+          glDrawArrays(GL_TRIANGLES,0,6);
+
+      }
+    }
 
     glBindVertexArray(0);
     glBindTexture(GL_TEXTURE_2D,0);
